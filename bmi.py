e06c4fab764b27bd5f32f67d006691557dbf94fe
peter <PTANSC.23@ichat.sp.edu.sg>
2024-10-22 16:23:55 +0800

    Allow user input for BMI

--- bmi.py
+++ bmi.py
@@ -1,3 +1,18 @@
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+
+def display_main_menu():
+    print("display_main_menu")
+
+def get_user_input():
+    height = input('Enter your Height (m) ')
+    weight = input('Enter your Weight (kg) ')
+    
+
+def calc_average():
+    print("calc_average") 
 
 def calculate_bmi(height, weight):
     print("Height = " + str(height))
@@ -11,5 +26,6 @@ def calculate_bmi(height, weight):
         print("Normal Weight")
     else:
         print("Over Weight")
-
+if __name__=="__main__":
+    main()
 calculate_bmi(weight=72, height=1.75)
