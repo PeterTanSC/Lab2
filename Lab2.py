2f095d557607bb4c35b5cb23c7e1a2a0eb0c90b1
peter <PTANSC.23@ichat.sp.edu.sg>
2024-10-25 17:16:29 +0800

    Adding Lab 2.2 code

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,26 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def main(temp_list):
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = []
+    num_list = get_user_input()
+    num_list = temp_list.split(",")
+    
+def display_main_menu():
+    print("display_main_menu")
+
+def get_user_input():
+    temp_list = input('Enter Temperatures: ')
+
+def calc_average_temperature(num_list):
+    total_temp = sum(num_list)
+    x = len(num_list)
+    average_temp = total_temp/x
+    print("Average Temperature: " + average_temp)
+
+    
+def find_min_max_temperature():
+
+def sort_temperature():
+
+def calc_median_temperature():
